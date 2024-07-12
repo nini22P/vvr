557a1038935d751e242cf648c13b4f5184da3659
22 <60903333+nini22P@users.noreply.github.com>
2024-07-12 16:58:57 +0800

    add service worker

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,5 +8,8 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
   },
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({ registerType: 'autoUpdate' })
+  ],
 })
